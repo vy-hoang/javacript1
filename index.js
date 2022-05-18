b62b92b19db93cc573cecd2af1db88c733612143
vy-hoang <candicehoang00@gmail.com>
2022-05-17 23:50:58 -0400

    setup environment for js

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-document.getElementById("counter").innerText = 5;
+//document.getElementById("counter").innerText = 5
+let myage = 1
+let dogage = 7
+let mydogage = myage * dogage
+console.log(mydogage);
